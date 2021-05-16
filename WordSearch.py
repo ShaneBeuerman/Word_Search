4f0086de84cfb238ecd8f0ff070f60886df35793
ShaneBeuerman <shanebeuerman@gmail.com>
2021-05-15 19:24:35 -0600

    Made button gird
    Made a grid of buttons based off the word search and filled the holes with random characters.

--- WordSearch.py
+++ WordSearch.py
@@ -1,5 +1,9 @@
 #Word Search
-
+import tkinter
+import random
+import string
+import time
+from tkinter import *
 board = []
 wordlist = []
 
@@ -7,13 +11,22 @@ def printArray(arr):
     for i in arr:
         print(i)
 
+def fillSpacesWithRandomLetters(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == '-':
+                arr[i][j] = random.choice(string.ascii_letters)                
+
 f = open("wordSearch.txt", "r")
 while(True):
     line = f.readline()
     if not line:
         break
     line = line.strip("\n")
-    board.append(line)
+    line = line.replace(" ", "")
+    board.append(list(line))
+printArray(board)
+fillSpacesWithRandomLetters(board)
 printArray(board)
 f = open("wordList.txt", "r")
 while(True):
@@ -24,4 +37,11 @@ while(True):
     wordlist.append(line)
 printArray(wordlist)
 
-    
+top = tkinter.Tk()
+buttons = []
+for i in range(10):
+    for j in range(10):
+        button = tkinter.Button(top, text=board[j][i])
+        buttons.append(button)
+        button.grid(row = j, column = i)
+top.mainloop()
