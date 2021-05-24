ba1942f992316d72bef99de6bddd3e69fea07b08
ShaneBeuerman <shanebeuerman@gmail.com>
2021-05-24 14:04:31 -0600

    Added button presses
    Updated the GUI to include a wordlist and made the button presses display the letters clicked on. Made other functions to be worked on later.

--- WordSearch.py
+++ WordSearch.py
@@ -15,7 +15,23 @@ def fillSpacesWithRandomLetters(arr):
     for i in range(len(arr)):
         for j in range(len(arr[i])):
             if arr[i][j] == '-':
-                arr[i][j] = random.choice(string.ascii_letters)                
+                arr[i][j] = random.choice(string.ascii_lowercase)
+
+def buttonPress(char):
+    global currentLetters
+    currentLetters += char
+    print(currentLetters)
+    updateGUI()
+
+def crossOff():
+    print("To be continued")
+
+def win():
+    print("N/A")
+
+def updateGUI():
+    global letterslabel
+    letterslabel.config(text="Current Letters: " + "\n" + currentLetters)
 
 f = open("wordSearch.txt", "r")
 while(True):
@@ -38,10 +54,21 @@ while(True):
 printArray(wordlist)
 
 top = tkinter.Tk()
-buttons = []
+buttons =  [[None]*10 for _ in range(10)]
+currentLetters = ""
 for i in range(10):
     for j in range(10):
-        button = tkinter.Button(top, text=board[j][i])
-        buttons.append(button)
-        button.grid(row = j, column = i)
+        letter = board[j][i]
+        button = tkinter.Button(top, text=letter, command = lambda cur=letter: buttonPress(cur))
+        buttons[j][i] = button
+        buttons[j][i].grid(row = j, column = i)
+words = []
+for i in range(len(wordlist)):
+    word = tkinter.Label(text=wordlist[i])
+    words.append(word)
+    word.grid(column = 11, row = i)
+
+letterslabel = tkinter.Label(text="Current letters: " + "\n" + currentLetters)
+letterslabel.grid(column = 12, row = 0)
+
 top.mainloop()
